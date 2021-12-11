e0f3d784725ec3e5fc405826e9f3dc048e30731c
Arthur Opriatnyi <imarrche@gmail.com>
2021-12-11 10:51:34 +0200

    Add TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST') ?? 'localhost',
+        port: configService.get('POSTGRES_PORT') ?? 5432,
+        username: configService.get('POSTGRES_USER') ?? 'username',
+        password: configService.get('POSTGRES_PASSWORD') ?? 'password',
+        database: configService.get('POSTGRES_DATABASE') ?? 'txdx',
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: configService.get('ENV') === 'dev',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
